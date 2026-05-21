78c216278a5bf7bf572dc2ca20c95742548d9ffc
LeonScottKennedy1998 <karina_alekseeva_2006@mail.ru>
2026-05-21 19:45:24 +0300

    фикс

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,18 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-
-    ssl:
-        process.env.DB_SSL === 'true' || process.env.DB_SSL === '1'
-            ? { rejectUnauthorized: false }
-            : false,
-
+    ssl: {
+        rejectUnauthorized: false
+    },
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 10000
@@ -23,12 +14,7 @@ pool.connect((err, client, release) => {
     if (err) {
         console.error('DB connect error:', err.message);
         console.error('Env:', {
-            hasDB_URL: !!process.env.DB_URL,
-            hasDB_USER: !!process.env.DB_USER,
-            hasDB_PASSWORD: !!process.env.DB_PASSWORD,
-            hasDB_HOST: !!process.env.DB_HOST,
-            hasDB_PORT: !!process.env.DB_PORT,
-            hasDB_NAME: !!process.env.DB_NAME,
+            hasDB_URL: !!process.env.DB_URL
         });
     } else {
         console.log('PostgreSQL connected');
